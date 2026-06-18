8da73d3e5666b035b7f897c98e932de070ef3aba
LambdaLight4205 <lambda.light@proton.me>
2026-06-18 17:17:49 +0200

    rows update

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 COLUMNS = [chr(i) for i in range(ord("a"), ord("d") + 1)]
-ROWS = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-]
+ROWS = [i for i in range(1, 17)]
 
 
 class Square:

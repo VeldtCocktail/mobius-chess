8ef9ace8b7e211843254783671bc96e1496c4ae0
LambdaLight4205 <lambda.light@proton.me>
2026-06-18 18:37:16 +0200

    var types

--- gui.py
+++ gui.py
@@ -4,9 +4,10 @@ from PyQt5.QtWidgets import (
     QWidget,
     QLayout
 )
+from main import Game
 
 class MainWindow(QMainWindow):
-    def __init__(self, game = None, debug = False):
+    def __init__(self, game:Game = None, debug:bool = False):
         super().__init__()
         self.game = game
         self.debug = debug
